6e6e3de5a0404eb75b9f0c30b2ad3cf410eacbb2
Merwan Guerrib <merwanguerrib@gmail.com>
2018-10-29 20:51:32 +0100

    done: pokemon.js reformated, poke-battle. html crafted, css basics ;

--- starter_code/javascript/pokemon.js
+++ starter_code/javascript/pokemon.js
@@ -1,90 +1,36 @@
-//TODO: créer les pokemons individuels en variable 
-
 // Attacks
 
 var attacksName = ["scratch","kick","smash","head charge"];
 
-var sounds = [];
-
-//function Attacks(attacksName,sound,damage) {
-//  this.name = attacksName[i];
-//  this.sound = sound[i];
-//  this.damage = damage;
-//};
-
 // Pokemon Constructor
 
-function Pokemon(name,health,sprite,attacksName,attack){
+function Pokemon(name,health,avatar,sound){
   this.name = name;
   this.health = health;
-  this.sprite = sprite;
-  this.attacks = attacksName;
-  //this.attack = function (){
-  //  return this.health
-  //};
-  //this.receiveDamage = function(damage){
-  //  this.health -= damage;
-  //}
+  this.avatar = avatar;
+  this.sound = sound;
   this.attackCry = function(){
     // Specify the attack ??
     return name + " has chossen to attack"
   }
 };
 
-
-Pokemon.prototype.receiveDamage = function(health,damage){
-  var damage = Math.floor(Math.random() * (health)); 
-  if (this.health > 0){
-    return name + " has received " + damage + " points of damage";
+Pokemon.prototype.attack = function(pokemon){
+  var damage = Math.floor(Math.random() * (pokemon.health));
+  pokemon.health -= damage;
+  var audio = new Audio(this.sound);
+  audio.play(); 
+  if (pokemonhis.health > 0){
+    return pokemon.name + " has received " + damage + " points of damage";
 }
-    else name + "has lost the battle!";
+    else pokemon.name + "has lost the battle!";
 };
 
 // Pokemon.prototype.attack = function // A définir
 
-
 // Pokemon Instances
-// chaque pokemon est une variable ex var pikachu = new pokemon (avec le name et le health etc.)
-Pikachu.prototype = Object.create(Pokemon.prototype);
-Pikachu.prototype.constructor = Pikachu;
 
-function Pikachu(name,health,sprite,attacksName){
-  Pokemon.call(this,health,sprite,attacksName);
-  this.name = name;
-  this.health = 100;
-  this.sprite = "../elements/sprites/pikachu.png";
-  this.attacksName = attacksName;
-};
-
-Charmander.prototype = Object.create(Pokemon.prototype);
-Charmander.prototype.constructor = Charmander;
-
-function Charmander(name,health,sprite,attacksName){
-  Pokemon.call(this,health,sprite,attacksName);
-  this.name = name;
-  this.health = 100;
-  this.sprite = "../elements/sprites/charmander.png";
-  this.attacksName = attacksName;
-};
-
-Squirtle.prototype = Object.create(Pokemon.prototype);
-Squirtle.prototype.constructor = Squirtle;
-
-function Squirtle(name,health,sprite,attacksName){
-  Pokemon.call(this,health,sprite,attacksName);
-  this.name = name;
-  this.health = 100;
-  this.sprite = "../elements/sprites/squirtle.png";
-  this.attacksName = attacksName;
-};
-
-Bulbasaur.prototype = Object.create(Pokemon.prototype);
-Bulbasaur.prototype.constructor = Bulbasaur;
-
-function Bulbasaur(name,health,sprite,attacksName){
-  Pokemon.call(this,health,sprite,attacksName);
-  this.name = name;
-  this.health = 100;
-  this.sprite = "../elements/sprites/bulbasaur.png";
-  this.attacksName = attacksName;
-};
+var pikachu = new Pokemon ("Pikachu",100,"../elements/sprites/pikachu.png","../elements/sounds/Pikachu-attack-sound.mp3");
+var charmander = new Pokemon("Charmander",100,"../elements/sprites/charmander.png","../elements/sounds/Charmander-attack-sound.mp3");
+var bulbasaur = new Pokemon ("Bulbasaur",100,"../elements/sprites/squirtle.png","../elements/sounds/Bulbasaur-attack-sound.mp3");
+var squirtle = new Pokemon("Squirtle",100,"../elements/sprites/bulbasaur.png","../elements/sounds/Squirtle-attack-sound.mp3");
