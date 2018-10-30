4e54a5f9890d1f5fc25e63b41b216615c298cb4c
Merwan Guerrib <merwanguerrib@gmail.com>
2018-10-30 21:36:20 +0100

    function left/right arrow

--- starter_code/javascript/main.js
+++ starter_code/javascript/main.js
@@ -2,32 +2,35 @@ $(document).ready(function(){
   console.log("ready!");
 
 
-$('#pikachu.characters').addClass('selected-char');
-var characters = ["pikachu","charmander","squirtle","bulbasaur"];
-var currentCharIndex = 0;
-var selectedId = "#" + characters[currentCharIndex] + ".characters"
-$(document).keydown(moveSelection);
+  $('#pikachu.characters').addClass('selected-char');
+  var characters = ["pikachu","charmander","squirtle","bulbasaur"];
+  var currentCharIndex = 0;
+  // var selectedId = "#" + characters[currentCharIndex] + ".characters";
+  var selectedId;
+  
 
-// right arrow = 39 
-// left arrow: 37 
+  $(document).keydown(function(event){
+console.log("event initiated");
+    switch (event.keyCode) {
+      case 37: // left arrow: 37 
+          currentCharIndex -= 1;
+          //selectedId = "#" + characters[currentCharIndex] + ".characters";
+          //$(selectedId).addClass('selected-char');
+          console.log("left arrow clicked");
+          console.log("currentCharIndex is " + currentCharIndex);
+          break;
 
-function moveSelection(event) {
-  console.log("event initiated");
-  switch (event.keyCode) {
-    case 37:
-    currentCharIndex -= 1;
-      break;
+      case 39: // right arrow = 39 
+          currentCharIndex += 1;
+          //selectedId = "#" + characters[currentCharIndex] + ".characters";
+         // $(selectedId).addClass('selected-char');
+         console.log("right arrow clicked");
+         console.log("currentCharIndex is " + currentCharIndex);
+          break;
+    }
+    console.log("event initiated3");
+  });
 
-    case 39:
-    currentCharIndex += 1;
-      break;
-  }
-  $(selectedId).addClass('selected-char');
-};
-
-
-//onkeydown --> j'enlève la classe selected-char de l'élément qui a currentCharIndex 
-// j'incrémente ou décrémente currentCharIndex et j'ajoute la classe selected-chars à characters [currentCharIndex]
 
 
 });  // fin du document.ready
