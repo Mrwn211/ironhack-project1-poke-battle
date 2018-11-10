9ddaa7ccc8acd1023e32259be5d7745c99916fac
Merwan Guerrib <merwanguerrib@gmail.com>
2018-11-10 14:40:16 +0100

    sounds access updated

--- javascript/pokemon.js
+++ javascript/pokemon.js
@@ -27,9 +27,9 @@ Pokemon.prototype.attack = function(pokemon){
 
 // Pokemon Instances
 
-var pikachu = new Pokemon ("pikachu",100,"../elements/sprites/pikachu.png","../elements/sounds/Pikachu-attack-sound.mp3");
-var charmander = new Pokemon("charmander",100,"../elements/sprites/charmander.png","../elements/sounds/Charmander-attack-sound.mp3");
-var bulbasaur = new Pokemon ("bulbasaur",100,"../elements/sprites/squirtle.png","../elements/sounds/Bulbasaur-attack-sound.mp3");
-var squirtle = new Pokemon("squirtle",100,"../elements/sprites/bulbasaur.png","../elements/sounds/Squirtle-attack-sound.mp3");
+var pikachu = new Pokemon ("pikachu",100,"./elements/sprites/pikachu.png","./elements/sounds/Pikachu-attack-sound.mp3");
+var charmander = new Pokemon("charmander",100,"./elements/sprites/charmander.png","./elements/sounds/Charmander-attack-sound.mp3");
+var bulbasaur = new Pokemon ("bulbasaur",100,"./elements/sprites/squirtle.png","./elements/sounds/Bulbasaur-attack-sound.mp3");
+var squirtle = new Pokemon("squirtle",100,"./elements/sprites/bulbasaur.png","./elements/sounds/Squirtle-attack-sound.mp3");
 
 var pokemons = [pikachu, charmander, squirtle, bulbasaur];
